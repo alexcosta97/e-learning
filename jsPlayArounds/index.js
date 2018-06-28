451302e00ec4cbd80e7fd3027e6542eb61193a75
Alexandre Costa <alexandrecosta@Alexandres-MacBook-Air.local>
2018-06-28 17:14:32 +0100

    feat: do vue.js intro

---
+++ jsPlayArounds/index.js
@@ -0,0 +1,34 @@
+const codes = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
+  
+  function init() {
+    let index = 0;
+    
+    document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    if(keyName === codes[index])
+    {
+      index++;
+      if(index === codes.length)
+      {
+        alert('Hurray!');
+        index = 0;
+      }
+    }
+    else
+    {
+      index = 0;
+    }
+  });
+  }
+  
